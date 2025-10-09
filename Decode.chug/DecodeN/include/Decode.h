0bef7577fc021c15ac8c79ac9eb83b969687d294
everett carpenter <65781323+SaintEverett@users.noreply.github.com>
2025-10-09 13:16:41 -0400

    DecodeN dropped

---
+++ Decode.chug/DecodeN/include/Decode.h
@@ -0,0 +1,66 @@
+#ifndef DECODEN_H
+#define DECODEN_H
+
+#include "chugin.h"
+#include "DecoderBase.h"
+
+template<const unsigned order_>
+class Decode : public Decoder<order_> // seems redundant, but essentially takes all decoder commonalities, gives to this "Basic decoder" class with a sampling tick function
+{
+public:
+
+	constexpr void tick(SAMPLE* in, SAMPLE* out, unsigned nframes) override
+	{
+		memset(out, 0, sizeof(SAMPLE) * n_channels * nframes); // clear
+		for (int f = 0; f < nframes; f++) // go through each frame
+		{
+			for (int c = 0; c < n_channels; c++) // go through each channel
+			{
+				SAMPLE sumd = 0;
+				for (int n = 0; n < n_channels; n++) // each channel of the incoming stream is a spherical harmonic
+				{
+					sumd += (channelBalance * in[f * n_channels + n] * SpeakSH[c][n]); // matrix mult of speaker SHs and input stream
+				}
+				out[f * n_channels + c] = sumd;
+			}
+		}
+	}
+};
+
+// this is actually what gets called and used by the chugin
+class Decode1 : public Decode<1> 
+{
+public:
+
+	Decode1(t_CKFLOAT fs) {};
+};
+
+class Decode2 : public Decode<2> 
+{
+public:
+
+	Decode2(t_CKFLOAT fs) {};
+};
+
+class Decode3 : public Decode<3> 
+{
+public:
+
+	Decode3(t_CKFLOAT fs) {};
+};
+
+class Decode4 : public Decode<4> 
+{
+public:
+
+	Decode4(t_CKFLOAT fs) {};
+};
+
+class Decode5 : public Decode<5> 
+{
+public:
+
+	Decode5(t_CKFLOAT fs) {};
+};
+
+#endif 
