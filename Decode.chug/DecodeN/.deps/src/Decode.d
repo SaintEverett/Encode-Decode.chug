dbaa37aec31b3c0fe2d691f69c7b8b44c274b0cf
heda <everettomatik@gmail.com>
2025-10-31 19:23:03 +0000

    make files working. linux and win32 tested. webchugin and mac are not tested

---
+++ Decode.chug/DecodeN/.deps/src/Decode.d
@@ -0,0 +1,8 @@
+src/Decode.o: src/Decode.cpp include/chugin.h include/Decode.h \
+ include/DecoderBase.h include/chugin.h include/chuckSH.h include/SHCT.h
+include/chugin.h:
+include/Decode.h:
+include/DecoderBase.h:
+include/chugin.h:
+include/chuckSH.h:
+include/SHCT.h:
---
+++ Decode.chug/SADN/.deps/src/SADN.d
@@ -0,0 +1,8 @@
+src/SADN.o: src/SADN.cpp include/chugin.h include/chuckSH.h \
+ include/SHCT.h include/SADN.h include/DecoderBase.h include/chugin.h
+include/chugin.h:
+include/chuckSH.h:
+include/SHCT.h:
+include/SADN.h:
+include/DecoderBase.h:
+include/chugin.h:
