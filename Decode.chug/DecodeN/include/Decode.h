b75ce5f5bc7e26fe7abc400ec969d9a5ec82c21b
everett carpenter <65781323+SaintEverett@users.noreply.github.com>
2025-10-31 14:40:07 -0400

    decode .h

--- Decode.chug/DecodeN/include/Decode.h
+++ Decode.chug/DecodeN/include/Decode.h
@@ -1,18 +1,16 @@
 #ifndef DECODEN_H
 #define DECODEN_H
 
-#include "chugin.h"
 #include "DecoderBase.h"
 
-template<const unsigned order_>
+template <const unsigned order_>
 class Decode : public Decoder<order_> // seems redundant, but essentially takes all decoder commonalities, gives to this "Basic decoder" class with a sampling tick function
 {
 public:
-
-	constexpr void tick(SAMPLE* in, SAMPLE* out, unsigned nframes) override
+	constexpr void tick(SAMPLE *in, SAMPLE *out, unsigned nframes) override
 	{
 		memset(out, 0, sizeof(SAMPLE) * n_channels * nframes); // clear
-		for (int f = 0; f < nframes; f++) // go through each frame
+		for (int f = 0; f < nframes; f++)					   // go through each frame
 		{
 			for (int c = 0; c < n_channels; c++) // go through each channel
 			{
@@ -28,39 +26,34 @@ public:
 };
 
 // this is actually what gets called and used by the chugin
-class Decode1 : public Decode<1> 
+class Decode1 : public Decode<1>
 {
 public:
-
 	Decode1(t_CKFLOAT fs) {};
 };
 
-class Decode2 : public Decode<2> 
+class Decode2 : public Decode<2>
 {
 public:
-
 	Decode2(t_CKFLOAT fs) {};
 };
 
-class Decode3 : public Decode<3> 
+class Decode3 : public Decode<3>
 {
 public:
-
 	Decode3(t_CKFLOAT fs) {};
 };
 
-class Decode4 : public Decode<4> 
+class Decode4 : public Decode<4>
 {
 public:
-
 	Decode4(t_CKFLOAT fs) {};
 };
 
-class Decode5 : public Decode<5> 
+class Decode5 : public Decode<5>
 {
 public:
-
 	Decode5(t_CKFLOAT fs) {};
 };
 
-#endif 
+#endif
